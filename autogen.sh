5c056ab0f845e550c9c1e4a1f1568df24360d3d2
Jesse van den Kieboom <jvanden@grijpc10.epfl.ch>
2009-06-04 17:47:11 +0200

    Use autoreconf in autogen

--- autogen.sh
+++ autogen.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
-
-PKG_NAME="cpg-network"
-
-(test -f $srcdir/configure.ac \
-  && test -f $srcdir/README \
-  && test -d $srcdir/cpg-network) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
-    exit 1
-}
-
-aclocal && autoconf && automake --add-missing
+autoreconf --install
