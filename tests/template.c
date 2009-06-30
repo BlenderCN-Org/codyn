3e9cfd18f063c468976098cb4e210e6c9dff70cc
Jesse van den Kieboom <jvanden@grijpc10.epfl.ch>
2009-06-30 15:00:46 +0200

    Added test for template

---
+++ tests/template.c
@@ -0,0 +1,29 @@
+#include <cpg-network/cpg-network.h>
+#include <cpg-network/cpg-debug.h>
+
+#include <stdlib.h>
+#include <glib/gprintf.h>
+
+int 
+main (int argc, char *argv[])
+{
+	g_type_init ();
+	
+	cpg_debug_add (CPG_DEBUG_TYPE_ERROR);
+
+	CpgNetwork *network = cpg_network_new_from_file ("template.cpg");
+	
+	if (!network)
+	{
+		g_error ("Could not open network");
+		exit(1);
+	}
+	
+	gchar *xml = cpg_network_write_to_xml (network);
+	
+	g_printf ("\n%s\n", xml);
+	g_free (xml);
+	
+	g_object_unref (network);
+	return 0;
+}
