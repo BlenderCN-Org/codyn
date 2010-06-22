9c452c14cd8d2787548e621aecc3fc0e3f0c8e3c
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2010-06-22 11:41:01 +0200

    Fixed some more indentation

--- cpg-network/cpg-function.h
+++ cpg-network/cpg-function.h
@@ -9,19 +9,19 @@
 
 G_BEGIN_DECLS
 
-#define CPG_TYPE_FUNCTION				(cpg_function_get_type ())
-#define CPG_FUNCTION(obj)				(G_TYPE_CHECK_INSTANCE_CAST ((obj), CPG_TYPE_FUNCTION, CpgFunction))
-#define CPG_FUNCTION_CONST(obj)			(G_TYPE_CHECK_INSTANCE_CAST ((obj), CPG_TYPE_FUNCTION, CpgFunction const))
-#define CPG_FUNCTION_CLASS(klass)		(G_TYPE_CHECK_CLASS_CAST ((klass), CPG_TYPE_FUNCTION, CpgFunctionClass))
-#define CPG_IS_FUNCTION(obj)			(G_TYPE_CHECK_INSTANCE_TYPE ((obj), CPG_TYPE_FUNCTION))
-#define CPG_IS_FUNCTION_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE ((klass), CPG_TYPE_FUNCTION))
-#define CPG_FUNCTION_GET_CLASS(obj)		(G_TYPE_INSTANCE_GET_CLASS ((obj), CPG_TYPE_FUNCTION, CpgFunctionClass))
+#define CPG_TYPE_FUNCTION            (cpg_function_get_type ())
+#define CPG_FUNCTION(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), CPG_TYPE_FUNCTION, CpgFunction))
+#define CPG_FUNCTION_CONST(obj)      (G_TYPE_CHECK_INSTANCE_CAST ((obj), CPG_TYPE_FUNCTION, CpgFunction const))
+#define CPG_FUNCTION_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), CPG_TYPE_FUNCTION, CpgFunctionClass))
+#define CPG_IS_FUNCTION(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), CPG_TYPE_FUNCTION))
+#define CPG_IS_FUNCTION_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), CPG_TYPE_FUNCTION))
+#define CPG_FUNCTION_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), CPG_TYPE_FUNCTION, CpgFunctionClass))
 
 CPG_FORWARD_DECL (CpgExpression);
 
-typedef struct _CpgFunction			CpgFunction;
-typedef struct _CpgFunctionClass	CpgFunctionClass;
-typedef struct _CpgFunctionPrivate	CpgFunctionPrivate;
+typedef struct _CpgFunction         CpgFunction;
+typedef struct _CpgFunctionClass    CpgFunctionClass;
+typedef struct _CpgFunctionPrivate  CpgFunctionPrivate;
 
 typedef struct _CpgFunctionArgument CpgFunctionArgument;
 
@@ -52,32 +52,33 @@ struct _CpgFunctionClass
 	                     CpgStack    *stack);
 };
 
-GType cpg_function_get_type (void) G_GNUC_CONST;
-GType cpg_function_argument_get_type (void) G_GNUC_CONST;
+GType                cpg_function_get_type                    (void) G_GNUC_CONST;
+GType                cpg_function_argument_get_type           (void) G_GNUC_CONST;
 
-CpgFunction *cpg_function_new (gchar const *name, gchar const *expression);
-CpgFunctionArgument *cpg_function_argument_new (gchar const *name, gboolean optional, gdouble def);
+CpgFunction         *cpg_function_new                         (gchar const         *name,
+                                                               gchar const         *expression);
+CpgFunctionArgument *cpg_function_argument_new                (gchar const         *name,
+                                                               gboolean             optional,
+                                                               gdouble              def);
+void                 cpg_function_add_argument                (CpgFunction         *function,
+                                                               CpgFunctionArgument *argument);
+void                 cpg_function_remove_argument             (CpgFunction         *function,
+                                                               CpgFunctionArgument *argument);
+void                 cpg_function_clear_arguments             (CpgFunction         *function);
+GList               *cpg_function_get_arguments               (CpgFunction         *function);
+guint                cpg_function_get_n_optional              (CpgFunction         *function);
+guint                cpg_function_get_n_arguments             (CpgFunction         *function);
+void                 cpg_function_execute                     (CpgFunction         *function,
+                                                               CpgStack            *stack);
+void                 cpg_function_set_expression              (CpgFunction         *function,
+                                                               CpgExpression       *expression);
 
-void cpg_function_add_argument (CpgFunction *function, CpgFunctionArgument *argument);
-void cpg_function_remove_argument (CpgFunction *function, CpgFunctionArgument *argument);
-
-void cpg_function_clear_arguments (CpgFunction *function);
-
-GList *cpg_function_get_arguments (CpgFunction *function);
-
-guint cpg_function_get_n_optional (CpgFunction *function);
-guint cpg_function_get_n_arguments (CpgFunction *function);
-
-void cpg_function_execute (CpgFunction *function, CpgStack *stack);
-
-void cpg_function_set_expression (CpgFunction *function, CpgExpression *expression);
 CPG_FORWARD_DECL (CpgExpression) *cpg_function_get_expression (CpgFunction *function);
 
-gchar const *cpg_function_argument_get_name (CpgFunctionArgument *argument);
-gboolean cpg_function_argument_get_optional (CpgFunctionArgument *argument);
-gdouble cpg_function_argument_get_default_value (CpgFunctionArgument *argument);
-
-CpgProperty *cpg_function_argument_get_property (CpgFunctionArgument *argument);
+gchar const         *cpg_function_argument_get_name           (CpgFunctionArgument *argument);
+gboolean             cpg_function_argument_get_optional       (CpgFunctionArgument *argument);
+gdouble              cpg_function_argument_get_default_value  (CpgFunctionArgument *argument);
+CpgProperty         *cpg_function_argument_get_property       (CpgFunctionArgument *argument);
 
 G_END_DECLS
 
