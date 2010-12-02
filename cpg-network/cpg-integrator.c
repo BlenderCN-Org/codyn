714f346f815caf44f805b99b4e7015aa059e63c8
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2010-12-02 09:10:16 +0100

    Fix calculation of direct links to sum

--- cpg-network/cpg-integrator.c
+++ cpg-network/cpg-integrator.c
@@ -495,15 +495,46 @@ simulation_step (CpgIntegrator *integrator)
 	while (direct)
 	{
 		CpgLinkAction *action = direct->data;
+		CpgProperty *target = cpg_link_action_get_target_property (action);
 
+		if (target)
+		{
+			cpg_property_set_update (target, 0);
+		}
+
+		direct = g_slist_next (direct);
+	}
+
+	direct = cpg_integrator_state_direct_link_actions (integrator->priv->state);
+
+	while (direct)
+	{
+		CpgLinkAction *action = direct->data;
 		CpgProperty *target = cpg_link_action_get_target_property (action);
 
 		if (target != NULL)
 		{
 			CpgExpression *expr = cpg_link_action_get_equation (action);
 
+			cpg_property_set_update (target,
+			                         cpg_property_get_update (target) +
+			                         cpg_expression_evaluate (expr));
+		}
+
+		direct = g_slist_next (direct);
+	}
+
+	direct = cpg_integrator_state_direct_link_actions (integrator->priv->state);
+
+	while (direct)
+	{
+		CpgLinkAction *action = direct->data;
+		CpgProperty *target = cpg_link_action_get_target_property (action);
+
+		if (target)
+		{
 			cpg_property_set_value (target,
-			                        cpg_expression_evaluate (expr));
+			                        cpg_property_get_update (target));
 		}
 
 		direct = g_slist_next (direct);
