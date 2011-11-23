7a7f11bdfc65a5f36d36780d8300975f56c5ad5d
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2011-11-23 10:04:27 +0100

    Improved usability of link contexts

--- cpg-network/cpg-parser-context.c
+++ cpg-network/cpg-parser-context.c
@@ -2825,9 +2825,6 @@ create_links_single (CpgParserContext          *context,
 		CpgSelection *tosel;
 		CpgExpansion *realid;
 		CpgSelection *obj;
-		CpgSelection *firstsel;
-		CpgSelection *secondsel;
-		gboolean argswitch;
 		gchar *newid;
 		gchar *uniq;
 
@@ -2838,21 +2835,6 @@ create_links_single (CpgParserContext          *context,
 
 		tosel = item->data;
 		item = g_slist_next (item);
-
-		if (bidi == NULL || idx % 2 == 1)
-		{
-			firstsel = fromsel;
-			secondsel = tosel;
-
-			argswitch = FALSE;
-		}
-		else
-		{
-			firstsel = tosel;
-			secondsel = fromsel;
-
-			argswitch = TRUE;
-		}
 
 		if (cpg_object_get_parent (cpg_selection_get_object (fromsel)) !=
 		    cpg_object_get_parent (cpg_selection_get_object (tosel)))
@@ -2887,8 +2869,17 @@ create_links_single (CpgParserContext          *context,
 
 		if (bidi != NULL)
 		{
-			gint widx = argswitch ? 1 : 0;
+			gint widx;
 			CpgEmbeddedString *s;
+
+			if (bidi == NULL || idx % 2 == 1)
+			{
+				widx = 0;
+			}
+			else
+			{
+				widx = 1;
+			}
 
 			s = CPG_EMBEDDED_STRING (cpg_attribute_get_argument (bidi, widx));
 
@@ -2915,10 +2906,13 @@ create_links_single (CpgParserContext          *context,
 		}
 
 		cpg_embedded_context_add_expansions (context->priv->embedded,
-		                                     cpg_selection_get_expansions (firstsel));
+		                                     cpg_selection_get_expansions (fromsel));
 
-		cpg_embedded_context_add_expansions (context->priv->embedded,
-		                                     cpg_selection_get_expansions (secondsel));
+		if (to)
+		{
+			cpg_embedded_context_add_expansions (context->priv->embedded,
+			                                     cpg_selection_get_expansions (tosel));
+		}
 
 		obj = parse_object_single_id (context,
 		                              realid,
