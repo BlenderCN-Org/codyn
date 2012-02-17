9753ee38ce774c07535bdc83291588a395b45920
Jeremie Knuesel <jeremie.knuesel@epfl.ch>
2012-02-17 11:40:47 +0100

    Emacs mode: fix derivatives' hilighting

--- data/codyn-mode.el
+++ data/codyn-mode.el
@@ -205,7 +205,7 @@
 
 (defvar codyn-font-lock-keywords
   (list
-   '("\\(\\(\\w\\|_\\)+\\)[ \t]*\\(([^(]*)[ \t]*\\)?\\(\\?=\\|<=\\|=\\)" 1 font-lock-variable-name-face)
+   '("\\(\\(\\w\\|_\\)+'*\\)[ \t]*\\(([^(]*)[ \t]*\\)?\\(\\?=\\|<=\\|=\\)" 1 font-lock-variable-name-face)
    '("@\\([0-9]+\\|[a-zA-Z_]\\(\\w\\|_\\)*\\)?\\(\\[[^[]*\\(\\[[^[]*\\]\\)?[^[]*\\]\\)*" . font-lock-constant-face)
    '("\\_<\\(?:in\\(?:tegrated\\)?\\|o\\(?:nce\\|ut\\)\\)\\_>"  . font-lock-type-face)
    '("\\_<\\(?:a\\(?:bove\\|ctions?\\|ll\\|n\\(?:cestors\\|y\\)\\|pply\\|[st]\\)\\|below\\|c\\(?:hildren\\|o\\(?:\\(?:ntex\\|un\\)t\\)\\)\\|d\\(?:e\\(?:bug\\(?:-print\\)?\\|fines?\\|lete\\|scendants\\)\\|isabled\\)\\|edges?\\|f\\(?:irst\\|rom\\(?:-set\\)?\\|unctions\\)\\|has-\\(?:flag\\|t\\(?:ag\\|emplate\\)\\)\\|i\\(?:f\\|mports?\\|n\\(?:clude\\|put\\(?:-name\\|s\\)?\\|te\\(?:grator\\|rface\\)\\)\\)\\|l\\(?:a\\(?:\\(?:s\\|you\\)t\\)\\|eft-of\\)\\|n\\(?:ame\\|o\\(?:des?\\|t\\)\\)\\|o\\(?:bjects?\\|utput\\(?:-name\\|s\\)?\\|[fn]\\)\\|p\\(?:ar\\(?:ent\\|se\\)\\|hase\\|iece\\|olynomial\\)\\|r\\(?:e\\(?:\\(?:cu\\|ve\\)rse\\)\\|ight-of\\|oot\\)\\|s\\(?:e\\(?:lf\\|t\\)\\|iblings\\|ubset\\)\\|t\\(?:e\\(?:mplates\\(?:-root\\)?\\|rminate\\)\\|o\\|ype\\)\\|un\\(?:apply\\|ique\\)\\|variables\\|w\\(?:hen\\|ith\\)\\)\\_>" . font-lock-keyword-face)
