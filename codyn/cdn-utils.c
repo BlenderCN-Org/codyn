5e3b2ad65372085b9e03ad9778d2fbdfcfa38787
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2012-02-20 12:39:45 +0100

    Better string to value conversion

--- codyn/cdn-utils.c
+++ codyn/cdn-utils.c
@@ -41,123 +41,355 @@ cdn_signal_accumulator_false_handled (GSignalInvocationHint *ihint,
 	return continue_emission;
 }
 
+static gboolean
+type_transform (GValue  *dest,
+                GType    gtype,
+                GError **error)
+{
+	GValue tmp = {0,};
+
+	if (G_VALUE_TYPE (dest) == gtype)
+	{
+		return TRUE;
+	}
+
+	if (!g_value_type_transformable (G_VALUE_TYPE (dest), gtype))
+	{
+		g_set_error (error,
+		             CDN_NETWORK_LOAD_ERROR,
+		             CDN_NETWORK_LOAD_ERROR_SYNTAX,
+		             "Failed to convert %s to %s",
+		             g_type_name (G_VALUE_TYPE (dest)),
+		             g_type_name (gtype));
+
+		g_value_unset (dest);
+		return FALSE;
+	}
+
+	g_value_init (&tmp, gtype);
+
+	if (!g_value_transform (dest, &tmp))
+	{
+		g_set_error (error,
+		             CDN_NETWORK_LOAD_ERROR,
+		             CDN_NETWORK_LOAD_ERROR_SYNTAX,
+		             "Failed to convert %s to %s",
+		             g_type_name (G_VALUE_TYPE (dest)),
+		             g_type_name (gtype));
+
+		g_value_unset (dest);
+		g_value_unset (&tmp);
+		return FALSE;
+	}
+
+	g_value_unset (dest);
+	g_value_init (dest, gtype);
+
+	g_value_copy (&tmp, dest);
+	g_value_unset (&tmp);
+
+	return TRUE;
+}
+
+static gboolean
+string_to_double (gchar const  *s,
+                  GType         gtype,
+                  GValue       *value,
+                  GError      **error)
+{
+	gdouble ret;
+	gchar *r;
+
+	ret = g_ascii_strtod (s, &r);
+
+	if (!r || *r)
+	{
+		g_set_error (error,
+		             CDN_NETWORK_LOAD_ERROR,
+		             CDN_NETWORK_LOAD_ERROR_SYNTAX,
+		             "Failed to convert `%s' to double",
+		             s);
+
+		return FALSE;
+	}
+
+	g_value_init (value, G_TYPE_DOUBLE);
+	g_value_set_double (value, ret);
+
+	return type_transform (value, gtype, error);
+}
+
+static gboolean
+string_to_int (gchar const  *s,
+               GType         gtype,
+               GValue       *value,
+               GError      **error)
+{
+	gint64 ret;
+	gchar *r;
+
+	ret = g_ascii_strtoll (s, &r, 10);
+
+	if (!r || *r)
+	{
+		g_set_error (error,
+		             CDN_NETWORK_LOAD_ERROR,
+		             CDN_NETWORK_LOAD_ERROR_SYNTAX,
+		             "Failed to convert `%s' to int",
+		             s);
+
+		return FALSE;
+	}
+
+	g_value_init (value, G_TYPE_INT64);
+	g_value_set_int64 (value, ret);
+
+	return type_transform (value, gtype, error);
+}
+
+static gboolean
+string_to_uint (gchar const  *s,
+                GType         gtype,
+                GValue       *value,
+                GError      **error)
+{
+	guint64 ret;
+	gchar *r;
+
+	ret = g_ascii_strtoull (s, &r, 10);
+
+	if (!r || *r)
+	{
+		g_set_error (error,
+		             CDN_NETWORK_LOAD_ERROR,
+		             CDN_NETWORK_LOAD_ERROR_SYNTAX,
+		             "Failed to convert `%s' to uint",
+		             s);
+
+		return FALSE;
+	}
+
+	g_value_init (value, G_TYPE_UINT64);
+	g_value_set_uint64 (value, ret);
+
+	return type_transform (value, gtype, error);
+}
+
+static gboolean
+string_to_boolean (gchar const  *s,
+                   GType         gtype,
+                   GValue       *value,
+                   GError      **error)
+{
+	g_value_init (value, G_TYPE_BOOLEAN);
+
+	g_value_set_boolean (value, g_ascii_strcasecmp (s, "true") == 0);
+
+	return type_transform (value, gtype, error);
+}
+
+static gboolean
+string_to_enum (gchar const  *s,
+                GType         gtype,
+                GValue       *value,
+                GError      **error)
+{
+	GEnumClass *cls;
+	GEnumValue *v;
+	gchar *r;
+	guint64 idx;
+
+	cls = g_type_class_ref (gtype);
+
+	idx = g_ascii_strtoull (s, &r, 10);
+
+	if (r && !*r)
+	{
+		v = g_enum_get_value (cls, (gint)idx);
+	}
+
+	if (!v)
+	{
+		v = g_enum_get_value_by_nick (cls, s);
+	}
+
+	if (!v)
+	{
+		v = g_enum_get_value_by_name (cls, s);
+	}
+
+	if (!v)
+	{
+		g_type_class_unref (cls);
+
+		g_set_error (error,
+		             CDN_NETWORK_LOAD_ERROR,
+		             CDN_NETWORK_LOAD_ERROR_SYNTAX,
+		             "Failed to convert `%s' to enum value",
+		             s);
+
+		return FALSE;
+	}
+
+	g_value_init (value, gtype);
+	g_value_set_enum (value, v->value);
+
+	g_type_class_unref (cls);
+
+	return TRUE;
+}
+
+static gboolean
+string_to_flags (gchar const  *s,
+                 GType         gtype,
+                 GValue       *value,
+                 GError      **error)
+{
+	GFlagsClass *cls;
+	GFlagsValue *v;
+	gchar *r;
+	guint64 idx;
+
+	cls = g_type_class_ref (gtype);
+
+	idx = g_ascii_strtoull (s, &r, 10);
+
+	if (r && !*r)
+	{
+		g_value_init (value, gtype);
+		g_value_set_flags (value, v->value);
+
+		g_type_class_unref (cls);
+		return TRUE;
+	}
+
+	if (!v)
+	{
+		v = g_flags_get_value_by_nick (cls, s);
+	}
+
+	if (!v)
+	{
+		v = g_flags_get_value_by_name (cls, s);
+	}
+
+	if (!v)
+	{
+		g_type_class_unref (cls);
+
+		g_set_error (error,
+		             CDN_NETWORK_LOAD_ERROR,
+		             CDN_NETWORK_LOAD_ERROR_SYNTAX,
+		             "Failed to convert `%s' to flags value",
+		             s);
+
+		return FALSE;
+	}
+
+	g_value_init (value, gtype);
+	g_value_set_flags (value, v->value);
+
+	g_type_class_unref (cls);
+
+	return TRUE;
+}
+
+static gboolean
+string_to_char (gchar const  *s,
+                GType         gtype,
+                GValue       *value,
+                GError      **error)
+{
+	if (!*s || *(s + 1))
+	{
+		g_set_error (error,
+		             CDN_NETWORK_LOAD_ERROR,
+		             CDN_NETWORK_LOAD_ERROR_SYNTAX,
+		             "Failed to convert `%s' to character",
+		             s);
+
+		return FALSE;
+	}
+
+	g_value_init (value, G_TYPE_CHAR);
+	g_value_set_char (value, *s);
+
+	return type_transform (value, gtype, error);
+}
+
+static gboolean
+string_to_string (gchar const  *s,
+                  GType         gtype,
+                  GValue       *value,
+                  GError      **error)
+{
+	g_value_init (value, G_TYPE_STRING);
+	g_value_set_string (value, s);
+
+	return type_transform (value, gtype, error);
+}
+
+typedef gboolean (*FromStringConversionFunc) (gchar const *, GType, GValue *, GError **);
+
+typedef struct
+{
+	GType type;
+	FromStringConversionFunc func;
+} FromStringConversionMap;
+
+static const FromStringConversionMap from_string_conv_map[] =
+{
+	{G_TYPE_BOOLEAN, string_to_boolean},
+	{G_TYPE_DOUBLE, string_to_double},
+	{G_TYPE_FLOAT, string_to_double},
+	{G_TYPE_STRING, string_to_string},
+	{G_TYPE_CHAR, string_to_char},
+	{G_TYPE_UCHAR, string_to_char},
+	{G_TYPE_INT, string_to_int},
+	{G_TYPE_INT64, string_to_int},
+	{G_TYPE_LONG, string_to_int},
+	{G_TYPE_UINT, string_to_uint},
+	{G_TYPE_UINT64, string_to_uint},
+	{G_TYPE_ULONG, string_to_uint}
+};
+
 gboolean
 cdn_string_to_value (gchar const  *s,
                      GType         type,
                      GValue       *value,
                      GError      **error)
 {
-	GValue sv = {0,};
+	gint i;
 
+	g_return_val_if_fail (s, FALSE);
 	g_return_val_if_fail (value != NULL, FALSE);
 
-	g_value_init (value, type);
-
-	if (type == G_TYPE_BOOLEAN)
+	for (i = 0; i < G_N_ELEMENTS (from_string_conv_map); ++i)
 	{
-		gint len = strlen (s);
-
-		// Custom transform from string to boolean
-		g_value_set_boolean (value,
-		                     g_ascii_strcasecmp (s, "true") == 0 ||
-		                     g_ascii_strcasecmp (s, "yes") == 0 ||
-		                     (len == 1 && *s == '1'));
-
-		return TRUE;
-	}
-	else if (type == G_TYPE_DOUBLE)
-	{
-		gdouble ret;
-		gchar *r;
-
-		ret = g_ascii_strtod (s, &r);
-
-		if (!r || *r)
+		if (from_string_conv_map[i].type == type)
 		{
-			g_set_error (error,
-			             CDN_NETWORK_LOAD_ERROR,
-			             CDN_NETWORK_LOAD_ERROR_SYNTAX,
-			             "Failed to convert `%s' to double",
-			             s);
-
-			return FALSE;
+			return from_string_conv_map[i].func (s,
+			                                     type,
+			                                     value,
+			                                     error);
 		}
-
-		g_value_set_double (value, ret);
-		return TRUE;
 	}
-	else if (type == G_TYPE_INT || type == G_TYPE_UINT ||
-	         type == G_TYPE_INT64 || type == G_TYPE_UINT64)
+
+	if (G_TYPE_IS_FLAGS (type))
 	{
-		gint64 ret;
-		gchar *r;
-
-		ret = g_ascii_strtoll (s, &r, 10);
-
-		if (!r || *r)
-		{
-			g_set_error (error,
-			             CDN_NETWORK_LOAD_ERROR,
-			             CDN_NETWORK_LOAD_ERROR_SYNTAX,
-			             "Failed to convert `%s' to integer",
-			             s);
-
-			return FALSE;
-		}
-
-		g_value_init (&sv, G_TYPE_INT64);
-		g_value_set_int64 (&sv, ret);
-
-		if (!g_value_transform (&sv, value))
-		{
-			g_value_unset (&sv);
-			g_value_unset (value);
-
-			g_set_error (error,
-			             CDN_NETWORK_LOAD_ERROR,
-			             CDN_NETWORK_LOAD_ERROR_SYNTAX,
-			             "Failed to convert `%s' to integer",
-			             s);
-
-			return FALSE;
-		}
-
-		g_value_unset (&sv);
-		return TRUE;
+		return string_to_flags (s, type, value, error);
 	}
-
-	if (!g_value_type_transformable (G_TYPE_STRING, type))
+	else if (G_TYPE_IS_ENUM (type))
 	{
-		g_value_unset (value);
-
-		g_set_error (error,
-		             CDN_NETWORK_LOAD_ERROR,
-		             CDN_NETWORK_LOAD_ERROR_SYNTAX,
-		             "String value `%s' cannot be converted to `%s'",
-		             s,
-		             g_type_name (type));
-
-		return FALSE;
+		return string_to_enum (s, type, value, error);
 	}
 
-	g_value_init (&sv, G_TYPE_STRING);
-	g_value_set_string (&sv, s);
+	g_set_error (error,
+	             CDN_NETWORK_LOAD_ERROR,
+	             CDN_NETWORK_LOAD_ERROR_SYNTAX,
+	             "String value `%s' cannot be converted to `%s'",
+	             s,
+	             g_type_name (type));
 
-	if (!g_value_transform (&sv, value))
-	{
-		g_value_unset (&sv);
-		g_value_unset (value);
-
-		g_set_error (error,
-		             CDN_NETWORK_LOAD_ERROR,
-		             CDN_NETWORK_LOAD_ERROR_SYNTAX,
-		             "Could not convert `%s' to `%s'",
-		             s,
-		             g_type_name (type));
-
-		return FALSE;
-	}
-
-	g_value_unset (&sv);
-	return TRUE;
+	return FALSE;
 }
