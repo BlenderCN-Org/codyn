aa707f429ab14777367f94af9fb0d4da0b071f79
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2013-04-30 18:02:03 +0200

    Fix python overrides for column major

--- codyn/Cdn.py
+++ codyn/Cdn.py
@@ -21,39 +21,25 @@ def flat_to_matrix(ret, dims):
         return ret
 
     rret = []
+    start = 0
 
-    for i in range(0, dims[0]):
-        start = i * dims[1]
-        end = start + dims[1]
+    for i in range(0, dims.columns]):
+        end = start + dims.rows
 
         rret.extend(ret[start:end])
+        start += dims.rows
 
     return rret
 
-class Variable(Cdn.Variable):
-    def get_values(self):
-        ret = self.get_values_flat()
+class Matrix(Cdn.Matrix):
+    @property
+    def values(self)
+        ret = self.get_flat()
         dims = self.get_dimension()
 
         return flat_to_matrix(ret, dims)
 
     def set_values(self, vals, numr, numc):
         self.set_values_flat(vals, numr, numc)
-
-Variable = override(Variable)
-__all__.append('Variable')
-
-class Expression(Cdn.Expression):
-    def get_values(self):
-        ret = self.get_values_flat()
-        dims = self.get_dimension()
-
-        return flat_to_matrix(ret, dims)
-
-    def set_values(self, vals, numr, numc):
-        self.set_values_flat(vals, numr, numc)
-
-Expression = override(Expression)
-__all__.append('Expression')
 
 # vi:ex:ts=4:et
